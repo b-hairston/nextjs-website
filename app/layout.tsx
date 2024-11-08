46f421373f2058e1fd05d8660e926e0580ac9572
b-hairston <bph484@gmail.com>
2024-11-08 13:44:53 -0600

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/react"
+
 export const metadata = {
   title: 'Hairston',
   description: 'Portfolio Site',
@@ -13,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      <Analytics />
     </html>
   )
 }
